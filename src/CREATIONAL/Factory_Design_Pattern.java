d676dd9164997e3c53d17f55f98b6beaf615d585
sheemabraza <sheemabraza42@gmail.com>
2026-02-08 00:12:25 +0530

    Adding Creational Pattern comments

--- src/CREATIONAL/Factory_Design_Pattern.java
+++ src/CREATIONAL/Factory_Design_Pattern.java
@@ -2,6 +2,8 @@ package CREATIONAL;
 
 public class Factory_Design_Pattern {
     public static void main(String[] args) {
+        // We only ask the factory for a Notification object.
+        // We do NOT create EmailNotification or SmsNotification directly here.
         Notification n =
                 NotificationFactory.getNotification("sms");
 
@@ -10,17 +12,31 @@ public class Factory_Design_Pattern {
 }
 
 
+/*
+ * Common interface.
+ * All notification types must follow this contract.
+ */
 interface Notification {
     void send(String msg);
 }
 
+/*
+ * One concrete implementation of Notification.
+ */
 class EmailNotification implements Notification {
+
+    @Override
     public void send(String msg) {
         System.out.println("Email: " + msg);
     }
 }
 
+/*
+ * Another concrete implementation of Notification.
+ */
 class SmsNotification implements Notification {
+
+    @Override
     public void send(String msg) {
         System.out.println("SMS: " + msg);
     }
